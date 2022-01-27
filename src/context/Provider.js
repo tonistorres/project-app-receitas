916a67ebc442478645bf773555d75090251a5b16
Helder Emanuel Alves de Oliveira <hlremanuel16@gmail.com>
2022-01-27 20:09:21 -0300

    commit to PR

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import context from './context';
+import { getMealsCategory, getDrinksCategory } from '../services/fetch';
 
 export default function Provider({ children }) {
+  const [foodCategory, setFoodCategory] = useState([]);
+  const [drinkCategory, setDrinkCategory] = useState([]);
+
+  async function setCategories() {
+    const drinks = await getDrinksCategory();
+    const foods = await getMealsCategory();
+    setFoodCategory(foods);
+    setDrinkCategory(drinks);
+  }
+
+  useEffect(() => {
+    setCategories();
+  }, []);
+
+  const value = {
+    foodCategory,
+    drinkCategory,
+  };
+
   return (
-    <context.Provider value={ context }>
+    <context.Provider value={ value }>
       {children}
     </context.Provider>
   );
--- src/services/fetch.js
+++ src/services/fetch.js
@@ -2,25 +2,25 @@ const ALL_DRINKS_CATEGORY = 'https://www.thecocktaildb.com/api/json/v1/1/list.ph
 const ALL_FOOD_CATEGORY = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
 
 export function getDrinksCategory() {
-  fetch(ALL_DRINKS_CATEGORY)
+  return fetch(ALL_DRINKS_CATEGORY)
     .then((r) => r.json())
     .then((data) => data.drinks.map((e) => e.strCategory));
 }
 
 export function filterDrinks(queue) {
-  fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${queue}`)
+  return fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${queue}`)
     .then((r) => r.json())
     .then((data) => data.drinks);
 }
 
 export function getMealsCategory() {
-  fetch(ALL_FOOD_CATEGORY)
+  return fetch(ALL_FOOD_CATEGORY)
     .then((r) => r.json())
     .then((data) => data.meals.map((e) => e.strCategory));
 }
 
 export function filterMeals(queue) {
-  fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${queue}`)
+  return fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${queue}`)
     .then((r) => r.json())
     .then((data) => data.meals);
 }
