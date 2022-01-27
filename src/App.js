3d0dfd26b219fa696696dd30586705fe38158ab1
Helder Emanuel Alves de Oliveira <hlremanuel16@gmail.com>
2022-01-27 19:03:18 -0300

    feat: Adds login page/ feat: Adds profile page/ style: adds login page initial style

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Switch } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import Login from './components/Login';
+import Foods from './pages/Foods';
+import Drinks from './pages/Drinks';
+import Explore from './pages/Explore';
+import Profile from './pages/Profile';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route path="/foods" component={ Foods } />
+          <Route path="/drinks" component={ Drinks } />
+          <Route path="/foods/:id" component={ Login } />
+          <Route path="/drinks/:id" component={ Login } />
+          <Route path="/foods/:id/in-progress" component={ Login } />
+          <Route path="/drinks/:id/in-progress" component={ Login } />
+          <Route path="/explore" component={ Explore } />
+          <Route path="/explore/foods" component={ Login } />
+          <Route path="/explore/drinks" component={ Login } />
+          <Route path="/explore/foods/ingredients" component={ Login } />
+          <Route path="/explore/drinks/ingredients" component={ Login } />
+          <Route path="/explore/foods/nationalities" component={ Login } />
+          <Route path="/profile" component={ Profile } />
+          <Route path="/done-recipes" component={ Login } />
+          <Route path="/favorite-recipes" component={ Login } />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
+export default function Profile() {
+  const [email, setEmail] = useState('');
+  const history = useHistory();
+  /* eslint-disable */
+  useEffect(() => {
+    const getEmailLocalStorage = JSON.parse(localStorage.getItem('user'));
+    if (getEmailLocalStorage) {
+      setEmail(getEmailLocalStorage.email);
+    }
+  }, []);
+   /* eslint-enable */
+  function logoutClick() {
+    localStorage.clear();
+    history.push('/');
+  }
+
+  return (
+    <div>
+      <p data-testid="profile-email">{email}</p>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={ () => history.push('/done-recipes') }
+      >
+        Done Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ () => history.push('/favorite-recipes') }
+      >
+        Favorite Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ () => logoutClick() }
+      >
+        Logout
+      </button>
+
+    </div>
+  );
+}
